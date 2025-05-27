828fa6d71336994ea4a976e9f627f27a25e75462
carlosgonza3 <crgonzalez1002@gmail.com>
2025-05-27 19:45:30 -0400

    Importing initial required libraries

---
+++ main.py
@@ -0,0 +1,14 @@
+
+# Required methods from scikit-learn
+from sklearn.datasets import load_digits
+from sklearn.model_selection import train_test_split
+
+# Required Libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Enhanced data visualization library based on matplotlib
+import seaborn as sns
+
+# Used for confusion metrix data
+from sklearn import metrics 
